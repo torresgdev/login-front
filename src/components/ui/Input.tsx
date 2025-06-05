bbb601757082c4332758e94a86a86279fa58f7a5
Gabriel Torres <torresg.dev@gmail.com>
2025-06-05 11:35:43 -0300

    created components/ui/input.tsx

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import type { HtmlHTMLAttributes, InputHTMLAttributes } from "react";
+
+// INTERFACE DE PROPRIEDADES (PROPS)
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> { 
+    label?: string;
+    error?: string;
+}
+
+// COMPONENTE INPUT
+const Input: React.FC<InputProps> = ({label, error, ...otherProps}) => {
+    return (
+        <div className="mb-4 w-full">
+
+            //LABEL
+
+            {label && (
+                <label htmlFor="{otherProps.id}" className="block text-gray-700 text-sm font-semibold mb-2">
+                    {label}
+                </label>
+            )}
+
+            // INPUT PRINCIPAL
+
+            <input {...otherProps}
+                className={`shadow-sm appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent
+                ${error ? 'border-red-500' : 'border-gray-300'}`}  />
+
+        // MENSAGEM DE ERRO
+        {error && (
+            <p className="text-red-800 text-sm mt-1">{error}</p>
+        )}  
+        </div>
+    )
+}
+
+export default Input;
