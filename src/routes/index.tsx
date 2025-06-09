ddee3544a2a91c77f825ceeb7291960211585af2
Gabriel Torres <torresg.dev@gmail.com>
2025-06-09 08:12:40 -0300

    created pages/DashboardPage.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
+import DashboardPage from "../pages/Dashboard";
 
 const AppRoutes: React.FC = () => {
     return(
@@ -12,6 +14,9 @@ const AppRoutes: React.FC = () => {
                 <Route path="/login" element={<LoginPage />} />
 
                 <Route path="/register" element={<RegisterPage />} />
+
+                <Route path="/dashboard" element={<DashboardPage />} />
+
             </Routes>
         </BrowserRouter>
     )
